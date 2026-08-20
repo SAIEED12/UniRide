1a7227dfe1193ce5f04c14c6810cfbbb0d895d0c
SAIEED12 <abusaieed0@gmail.com>
2026-08-20 22:22:35 +0600

    feat: add spacing for improved readability in Signup page

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+
+
 import React, { useState } from "react";
 import { ArrowLeft, Mail, CreditCard, User, Lock, Minus, Plus, Check } from "lucide-react";
 import Link from "next/link";
